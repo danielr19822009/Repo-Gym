a461c03f50fe28721965c15591b3f201ce2a2b30
danielr19822009 <115900087+danielr19822009@users.noreply.github.com>
2024-10-15 20:01:31 -0500

    Add files via upload

--- vista/menuprincipal.php
+++ vista/menuprincipal.php
@@ -1,115 +1,126 @@
+<?php
+require_once("../conexion/conexion.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Suponiendo que envías un formulario de inicio de sesión con 'username' y 'password'
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Consulta a la base de datos para verificar el usuario
+    $query = "SELECT * FROM usuarios WHERE nombre = ?"; // Cambia 'usuarios' por tu tabla
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+
+        // Verificar la contraseña (usa password_verify si las contraseñas están hasheadas)
+        if ($password === $user['password']) {
+            // Iniciar la sesión
+            session_start();
+            $_SESSION['usuario'] = $user['nombre']; // O el ID del usuario, etc.
+            header("Location: ../menuprincipal.php");
+            exit();
+        } else {
+            echo "Contraseña incorrecta.";
+			header("Location: /index.php");
+        }
+    } else {
+        echo "Usuario no encontrado.";
+    }
+}
+?>
+
+
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-	<meta charset="utf-8">
-	<title>Menu</title>
-	<link rel="stylesheet" type="text/css" href="..\stilos\estilos.css">
-
-
-		<link rel="stylesheet" href="..\plugins\sliderTabs-1.1\styles/jquery.sliderTabs.min.css">
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-		<script src="..\plugins\sliderTabs-1.1\jquery.sliderTabs.min.js"></script>
-
-
-		 <meta name="viewport" content="width=device-width, initial-scale=1">
-		  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-		  
-
+    <meta charset="utf-8">
+    <title>Menu</title>
+    <link rel="stylesheet" type="text/css" href="/css/estilos.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-	<body>
 
-			<table class="cont-bar-bienvenido">
-				<tr>
-					<td class="sessionnombre">
-						<?php
+<body class="bg-dark text-white">
 
-							@$mensaje = $_GET['mensaje'];
-							echo '<img src="..\imagenes\PNG\32x32\bullhorn-2.png">';
-							echo $mensaje;
-						 ?>
-					</td>
+    <div class="container mt-4">
+       
+            <?php
+                $mensaje = $_GET['mensaje'] ?? '';
+                if (!empty($mensaje)) {
+                    echo '<div class="alert alert-success">
+							<a href="#" aria-label="close">&times;</a>
+							<strong>'.htmlspecialchars($mensaje).'!</strong>
+						</div>';
+                }
+            ?>
 
-					<td class="nivel">
-						<?php
-							@$mensaje1 = $_GET['mensaje1'];
-							echo $mensaje1;
-						 ?>	
-					</td>
-				</tr>
-			</table>
-			
-			<div class="cont-menuprincipal">
+        </div>
+    </div>
 
-				<nav class="menu">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Muscle Factory</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="rutinaDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="../imagenes/PNG/32x32/user.png"> Rutina
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="rutinaDropdown">
+                            <li><a class="dropdown-item" href="menuprincipal.php?page=pecho"><img src="../imagenes/varias/16x16/Weightlifter_silhouette_16.png"> Pecho</a></li>
+                            <li><a class="dropdown-item" href="menuprincipal.php?page=triceps"><img src="../imagenes/varias/16x16/Weightlifter_silhouette_16.png"> Triceps</a></li>
+                            <li><a class="dropdown-item" href="#"><img src="../imagenes/varias/16x16/Weightlifter_upper_side_silhouette_16.png"> Biceps</a></li>
+                            <li><a class="dropdown-item" href="#"><img src="../imagenes/varias/16x16/Olympic_Weightlifting__16.png"> Espalda</a></li>
+                            <li><a class="dropdown-item" href="menuprincipal.php?page=pierna"><img src="../imagenes/varias/16x16/Weightlifter_silhouette_16.png"> Pierna</a></li>
+                            <li><a class="dropdown-item" href="#"><img src="../imagenes/varias/16x16/Standing_man_silhouette_lifting_dumbbells_16.png"> Hombros</a></li>
+                            <li><a class="dropdown-item" href="#"><img src="../imagenes/varias/16x16/Standing_man_silhouette_lifting_dumbbells_16.png"> Trapecio</a></li>
+                            <li><a class="dropdown-item" href="#"><img src="../imagenes/varias/16x16/Standing_man_silhouette_lifting_dumbbells_16.png"> Brazos</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="#"><img src="../imagenes/PNG/32x32/paragraph-justify-2.png"> Dietas</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#"><img src="../imagenes/PNG/32x32/home.png"> Clientes</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#"><img src="../imagenes/PNG/32x32/home.png"> Productos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#"><img src="../imagenes/PNG/32x32/home.png"> F.A.Q</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#"><img src="../imagenes/PNG/32x32/home.png"> Contáctenos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?page=cerrarsession"><img src="../imagenes/varias/32x32/Emergency_exit_32.png"> Salir</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-					<ul class="submenu">
-						<li><a href="menuprincipal.php"><img src="..\imagenes\PNG\32x32\user.png">Rutina</a>
-							<ul>
-								<li><a href="menuprincipal.php?page=pecho"><img src="..\imagenes\varias\16x16\Weightlifter_silhouette_16.png">Pecho</a></li>
-								<li><a href="menuprincipal.php?page=triceps"><img src="..\imagenes\varias\16x16\Weightlifter_silhouette_16.png">Triceps</a></li>
-								<li><a href=""><img src="..\imagenes\varias\16x16\Weightlifter_upper_side_silhouette_16.png">Biceps</a></li>
-								<li><a href=""><img src="..\imagenes\varias\16x16\Olympic_Weightlifting__16.png">Espalda</a></li>
-								<li><a href="menuprincipal.php?page=pierna"><img src="..\imagenes\varias\16x16\Weightlifter_silhouette_16.png">Pierna</a></li>
-								<li><a href=""><img src="..\imagenes\varias\16x16\Standing_man_silhouette_lifting_dumbbells_16.png">Hombros</a></li>
-								<li><a href=""><img src="..\imagenes\varias\16x16\Standing_man_silhouette_lifting_dumbbells_16.png">Trapecio</a></li>
-								<li><a href=""><img src="..\imagenes\varias\16x16\Standing_man_silhouette_lifting_dumbbells_16.png">Brazos</a></li>
-							</ul>
-						</li>
-						<li><a href=""><img src="..\imagenes\PNG\32x32\paragraph-justify-2.png">Dietas</a></li>
-						<li><a href=""><img src="..\imagenes\PNG\32x32\home.png">Clients</a></li>
-						<li><a href=""><img src="..\imagenes\PNG\32x32\home.png">Products</a></li>
-						<li><a href=""><img src="..\imagenes\PNG\32x32\home.png">F.A.Q</a></li>
-						<li><a href=""><img src="..\imagenes\PNG\32x32\home.png">Contactenos</a></li>
-						<li><a href="?page=cerrarsession"><img src="..\imagenes\varias\32x32\Emergency_exit_32.png">Salir</a></li>
+    <div class="container mt-4">
+        <div class="verpaginas">
+            <?php 
+                @$page = $_REQUEST["page"];
+                switch ($page) {
+                    case 'pecho':
+                        include_once("pecho/pecho.php");
+                        break;
+                    case 'triceps':
+                        include_once("triceps/triceps.php");
+                        break;
+                    case 'biceps':
+                        include_once("/biceps/biceps.php");
+                        break;
+                    case 'pierna':
+                        include_once("pierna/pierna.php");
+                        break;
+                    case 'cerrarsession':
+                        include_once("../validacion/cerrarsession.php");
+                        break;
+                }
+            ?>
+        </div>
+    </div>
 
-					</ul>
-				</nav>
-				
-			</div>
-
-
-
-		
-
-			<!-- VER PAGINAS-->
-			<div class="verpaginas">
-				<?php 
-					@$page = $_REQUEST["page"];
-					switch ($page){
-
- 						case 'pecho':
- 							include_once("pecho/pecho.php");
- 						break;
-	
- 						case 'triceps':
- 							include_once("triceps/triceps.php");
- 						break;
-
- 						case 'biceps':
- 							include_once("/biceps/biceps.php");
- 						break;
-
- 						case 'pierna':
- 							include_once("pierna/pierna.php");
- 						break;
-
- 						case 'cerrarsession':
- 							include_once("/../validacion/cerrarsession.php");
- 						break;
- 						
- 					}
-
-				 ?>
-			</div>
-
-		</div>
-
-	
-
-	</body>
-
-
+</body>
 </html>
